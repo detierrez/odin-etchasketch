85e9765e5b2f3eeeba2abeeaaff764c0ad7bc81d
detierrez <detierrez@gmail.com>
2025-05-05 21:13:33 -0400

    Add stylization

--- code.js
+++ code.js
@@ -1,26 +1,32 @@
-const btnReset = document.querySelector("#reset");
-const btnResize = document.querySelector("#resize");
-const btnRainbow = document.querySelector("#rainbow");
-const btnScratch = document.querySelector("#scratch");
+const btnClear = document.querySelector(".btn[data-label=Clear]");
+const btnClassic = document.querySelector(".btn[data-label=Classic]");
+const btnRainbow = document.querySelector(".btn[data-label=Rainbow]");
+const btnGradient = document.querySelector(".btn[data-label=Gradient]");
+const inputSize = document.querySelector("input");
 const divContainer = document.querySelector("#container");
 const containerSize = divContainer.clientHeight;
 const currentRainbowColor = [randInt(256), randInt(256), randInt(256)];
 
-let gridSize = 8;
-let isRainbowOn = true;
-let isScratchOn = false;
+let gridSize = 16;
+let isRainbowOn = false;
+let isGradientOn = false;
 
-btnReset.addEventListener("click", generateGrid);
-btnResize.addEventListener("click", updateGrid);
+btnClassic.addEventListener("click", activateClassic);
+btnGradient.addEventListener("click", toggleGradient);
 btnRainbow.addEventListener("click", toggleRainbow);
-btnScratch.addEventListener("click", toggleScratch);
+btnClear.addEventListener("click", generateGrid);
+inputSize.addEventListener("keyup", updateGrid);
 
 generateGrid();
 
-function updateGrid() {
-  gridSize = +prompt("Enter a new size: ");
-  gridSize = Math.min(100, gridSize);
-  generateGrid();
+function updateGrid(event) {
+  if (event.key === "Enter") {
+    gridSize = +inputSize.value;
+    gridSize = isNaN(gridSize) ? 0 : gridSize;
+    gridSize = bound(gridSize, 4, 100);
+    generateGrid();
+    inputSize.value = "";
+  }
 }
 
 function generateGrid() {
@@ -35,38 +41,49 @@ function generateGrid() {
     tiles.push(tile);
   }
   divContainer.replaceChildren(...tiles);
+
+  inputSize.placeholder = `${gridSize}x${gridSize}`;
 }
 
 function paint() {
   let [r, g, b, a] = toColorArray(this.style.backgroundColor);
-  if (!isRainbowOn && !isScratchOn) {
+  if (!isRainbowOn && !isGradientOn) {
     a = 0;
-  } 
-  
-  if (isRainbowOn) {
-    tickRainbow();
-    [r, g, b] = currentRainbowColor;
-  } 
-  
-  if (isScratchOn) {
+  }
+  if (isGradientOn) {
     if (!a) a = 1;
     a *= 0.7;
   }
+  if (isRainbowOn) {
+    tickRainbow();
+    [r, g, b] = currentRainbowColor;
+  }
+
   this.style.backgroundColor = toColorString([r, g, b, a]);
 }
 
+// Change the current rainbow color to a slightly different one
 function tickRainbow() {
   currentRainbowColor.forEach(
     (value, i) => (currentRainbowColor[i] = deviate(value))
   );
 }
 
-function toggleScratch() {
-  isScratchOn = !isScratchOn;
+function activateClassic() {
+  isGradientOn = false;
+  isRainbowOn = false;
+  btnGradient.classList.remove('active');
+  btnRainbow.classList.remove('active');
+}
+
+function toggleGradient() {
+  isGradientOn = !isGradientOn;
+  btnGradient.classList.toggle('active');
 }
 
 function toggleRainbow() {
   isRainbowOn = !isRainbowOn;
+  btnRainbow.classList.toggle('active');
 }
 
 function toColorString([r, g, b, a = 1]) {
